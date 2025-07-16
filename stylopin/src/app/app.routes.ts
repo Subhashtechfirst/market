8c1c55ab616d8f356d1b8ca07b6a6de1cf3efda4
Subhashtechfirst <warkadsubhash9@gmail.com>
2025-07-16 23:36:45 +0530

    login page changes

--- stylopin/src/app/app.routes.ts
+++ stylopin/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { UpdateAboutComponent } from './manage/update-about/update-about.compone
 import { UpdateBannerComponent } from './manage/update-banner/update-banner.component';
 import { UpdateBuyNowComponent } from './manage/update-buy-now/update-buy-now.component';
 import { UpdateContactComponent } from './manage/update-contact/update-contact.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path:"", component:ProductListComponent},
@@ -14,5 +15,6 @@ export const routes: Routes = [
     {path:"Banner", component:UpdateBannerComponent},
     {path:"Product", component:DashboardComponent},
     {path:"BuyNow", component:UpdateBuyNowComponent},
+    {path:"Login", component:LoginComponent},
     {path:"ContactUS", component:UpdateContactComponent},
 ];
